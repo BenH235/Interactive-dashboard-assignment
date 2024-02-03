683dfdc2b2a555d71fbb4c1d4412eb938f5db158
BenH235 <benholdsworth@live.co.uk>
2024-02-03 13:13:30 +0000

    updated app

--- app.py
+++ app.py
@@ -16,6 +16,7 @@ from haversine import haversine, Unit
 import datapoint
 from config import API_KEY
 
+
 # Utility functions
 def miles_to_meters(miles):
     return miles * 1609.34
@@ -36,7 +37,17 @@ st.title('Local Nature Reserve Finders')
 st.caption('An application that helps you find local nature reserves in England. The application suggests a reserve based on locality and short term weather forecasts.')
 
 gdf = fetch_geojson()
-postcode = st.text_input('Please enter valid postcode', value="", max_chars=None, key=None, type="default", help=None)
+
+mapping, information = st.columns((3, 2))
+
+first_input, second_input, third_input = mapping.columns(3)
+postcode = first_input.text_input('Please enter valid postcode', value="", max_chars=None, key=None, type="default", help=None)
+distance_miles = second_input.number_input('Please enter distance (in miles)', min_value=0, max_value=100, value=15)
+map_settings = third_input.expander('Map settings', expanded=False)
+
+map_width= map_settings.slider(label = 'Modify map width', min_value=500, max_value=3000, value=1000)
+map_height = map_settings.slider(label = 'Modify map height', min_value=500, max_value=3000, value=600)
+base_map = map_settings.selectbox(label ='Select basemap', options = ['OpenStreetMap', 'CartoDB Positron', 'CartoBD Voyager', 'NASAGIBS Blue Marble'], index=0)
 
 loc = requests.get(f'https://api.postcodes.io/postcodes/{postcode}').json()
 lat = loc['result']['latitude']
@@ -51,6 +62,7 @@ def distance_from_poly(polygon, start_lat=lat, start_lon=lon):
 def dataframe_with_selections(df):
     df_with_selections = df.copy()
     df_with_selections.insert(0, "Show weather forecast", False)
+    df_with_selections.loc[0, "Show weather forecast"] = True
 
     # Get dataframe row-selections from user with st.data_editor
     edited_df = st.data_editor(
@@ -58,6 +70,9 @@ def dataframe_with_selections(df):
         hide_index=True,
         column_config={"Select": st.column_config.CheckboxColumn(required=True)},
         disabled=df.columns,
+        use_container_width=True,
+        height = 220
+        
     )
 
     # Filter the dataframe using the temporary column, then drop the column
@@ -66,54 +81,15 @@ def dataframe_with_selections(df):
     
 
 gdf['distance'] = gdf['geometry'].apply(distance_from_poly)
-nearby_parks = gdf[gdf['distance'] <= 10]
-
-mapping, information = st.columns(2)
+nearby_parks = gdf[gdf['distance'] <= distance_miles]
 
 nearby_parks = nearby_parks[['NNR_NAME', 'distance', 'Shape__Area']].rename(columns  = {'NNR_NAME': 'Name', 'distance':'distance (miles)',  'Shape__Area': 'Land area'})
 
-# information.write(nearby_parks.sort_values(by = 'distance (miles)', ascending = True).reset_index(drop=True))
 
-with information:
-    selection = dataframe_with_selections(nearby_parks.sort_values(by = 'distance (miles)', ascending = True).reset_index(drop=True))
-
-locations = gdf[gdf.NNR_NAME.isin(selection.Name.to_list())]['geometry'].centroid.to_list()
-
-# Get weather data for selected locations
-# Create connection to DataPoint with your API key
-conn = datapoint.connection(api_key=API_KEY)
-
-# Get the nearest site for my latitude and longitude
-site = conn.get_nearest_forecast_site(locations[0].y, locations[0].x)
-
-# Get a forecast for my nearest site with 3 hourly timesteps
-forecast = conn.get_forecast_for_site(site.id, "daily")
-
-date_list = []
-text_list = []
-temp_list = []
-prec_list = []
-for day in forecast.days:
-
-    # Loop through time steps and print out info
-    for timestep in day.timesteps:
-        date_list.append(timestep.date)
-        text_list.append(timestep.weather.text)
-        temp_list.append(timestep.temperature.value)
-        prec_list.append(timestep.precipitation.value)
-        
-forecast_df=pd.DataFrame()
-forecast_df['date']=date_list
-forecast_df['text']=text_list
-forecast_df['tempurature']=temp_list
-forecast_df['precipitation']=prec_list
-
-information.write(forecast_df)
-
-m = folium.Map(tiles='OpenStreetMap', location=(50, 3), zoom_start=6)
+m = folium.Map(tiles=base_map, location=(lat, lon), zoom_start=9)
 folium.Marker(location=(lat, lon), popup="Central Point").add_to(m)
 
-radius_miles = miles_to_meters(20)
+radius_miles = miles_to_meters(distance_miles)
 
 # Create a CircleMarker to represent the distance
 circle_marker = folium.Circle(
@@ -122,7 +98,7 @@ circle_marker = folium.Circle(
     color='green',
     fill=False,
     dash_array='3',  # Set dash_array for a dotted line
-    popup="5000 meter radius",
+    popup=f"{distance_miles} radius",
     z_index=0
 )
 
@@ -147,15 +123,16 @@ tooltip = folium.GeoJsonTooltip(
     """,
     max_width=800,
 )
-condition = lambda feature: 'green' if feature['properties']['distance']<20 else 'blue'
+condition = lambda feature: 'blue' if feature['properties']['distance']<distance_miles else 'red'
 
 folium.GeoJson(gdf.to_json(), name='geojson_layer', 
     tooltip=tooltip,
     popup=popup, style_function=lambda feature: {
         'fillColor': condition(feature),
-        'fillOpacity': 0.75,
+        'fillOpacity': 0.9,
+        'color': 'grey',
+        'weight': 0.1
     },).add_to(m)
-
 
 
 circle_marker.add_to(m)
@@ -163,70 +140,54 @@ circle_marker.add_to(m)
 folium.LayerControl().add_to(m)
 
 with mapping:
-    folium_static(m, width=800)
+    folium_static(m, width=int(map_width),height=int(map_height))
 
 
 
+with information:
+    selection = dataframe_with_selections(nearby_parks.sort_values(by = 'distance (miles)', ascending = True).reset_index(drop=True))
 
-# st.write(cliked_lat, clicked_lon)
+locations = gdf[gdf.NNR_NAME.isin(selection.Name.to_list())]['geometry'].centroid.to_list()
 
-
-# if cliked_lat is not None:
-#     map_container.empty()
-    
-#     m = folium.Map(tiles='OpenStreetMap', location=(50, 3), zoom_start=6)
-#     cliked_lat = st_data["last_clicked"]['lat']
-#     clicked_lon = st_data["last_clicked"]['lng']
-    
-#     folium.Marker(location=(cliked_lat, clicked_lon), popup="Central Point").add_to(m)
-
-#     # Create a CircleMarker to represent the distance
-#     circle_marker = folium.Circle(
-#         location=(cliked_lat, clicked_lon),
-#         radius=50,
-#         color='blue',
-#         fill=True,
-#         fill_color='blue',
-#         fill_opacity=0.1,
-#         popup="5000 meter radius"
-#     )
-#     circle_marker.add_to(m)
-
-#     geojson_data = fetch_geojson()
-#     folium.GeoJson(geojson_data, name='geojson_layer').add_to(m)
+# Get weather data for selected locations
+# Create connection to DataPoint with your API key
+conn = datapoint.connection(api_key=API_KEY)
 
 
-#     folium_static(m, width=800)
-    
-    
+locs_forecast = []
+for locs, name in zip(locations, selection.Name.to_list()):
+# Get the nearest site for my latitude and longitude
+    site = conn.get_nearest_forecast_site(locs.y, locs.x)
 
-# # try:
-    
-#     cliked_lat = st_data["last_clicked"]['lat']
-#     clicked_lon = st_data["last_clicked"]['lng']
-    
-#     folium.Marker(location=(cliked_lat, clicked_lon), popup="Central Point").add_to(m)
+    # Get a forecast for my nearest site with 3 hourly timesteps
+    forecast = conn.get_forecast_for_site(site.id, "daily")
+    date_list = []
+    text_list = []
+    temp_list = []
+    prec_list = []
+    for day in forecast.days:
 
-#     # Create a CircleMarker to represent the distance
-#     circle_marker = folium.CircleMarker(
-#         location=(cliked_lat, clicked_lon),
-#         radius=5000,
-#         color='blue',
-#         fill=True,
-#         fill_color='blue',
-#         fill_opacity=0.2,
-#         popup="5000 meter radius"
-#     )
-#     circle_marker.add_to(m)
-#     with placeholder.container():
-#         st_folium(m, width=800, height = 500)
-# try:
-#     placeholder.empty()
-#     st.info('Click a point on the map to get started', icon="ℹ️")
-#     with placeholder.container():
-#         st_folium(m, width=800)
-# except:
-#     pass
+        # Loop through time steps and print out info
+        for timestep in day.timesteps:
+            date_list.append(timestep.date)
+            text_list.append(timestep.weather.text)
+            temp_list.append(timestep.temperature.value)
+            prec_list.append(timestep.precipitation.value)
+            
+    forecast_df=pd.DataFrame()
+    forecast_df['date']=date_list
+    forecast_df['text']=text_list
+    forecast_df['tempurature']=temp_list
+    forecast_df['precipitation']=prec_list
+    forecast_df['location'] = name
+
+    locs_forecast.append(forecast_df)
+
+forecast_df = pd.concat(locs_forecast)
+
+fig = px.line(forecast_df, x ='date', y='tempurature', color='location')
+information.plotly_chart(fig, use_container_width=True)
+
 
 
 
