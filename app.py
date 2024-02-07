b0df1c1db68068d0e188521c8192eba57685c890
BenH235 <benholdsworth@live.co.uk>
2024-02-07 20:16:28 +0000

    Add comments/docstrings

--- app.py
+++ app.py
@@ -21,26 +21,39 @@ from config import API_KEY
 def miles_to_meters(miles):
     '''
     Convert miles to meters
+    Args:
+        miles (float): Distance in miles
+    Returns:
+        float: Distance in meters
     '''
     return miles * 1609.34
 
 def distance_from_poly(polygon, start_lat, start_lon):
     '''
     Find distance from point to polygon centroid
+    Args
+        Polygon: Shape from which centroid will be calculated
+        start_lat (float): Latitude
+        start_lon (float): Longitude
+    Returns
+        float: Haversine distance
     '''
     end_lat = polygon.centroid.y
     end_lon = polygon.centroid.x
-    return haversine((start_lat, start_lon), (end_lat, end_lon), unit=Unit.MILES)
+    return round(haversine((start_lat, start_lon), (end_lat, end_lon), unit=Unit.MILES), 2)
 
 def dataframe_with_selections(df):
     '''
     Create dataframe with selection column
+    Args
+        df (pandas.DataFrame): Input dataframe
+    Returns
+        pandas.DataFrame: With addition "Show weather forecast" column.
     '''
     df_with_selections = df.copy()
     df_with_selections.insert(0, "Show weather forecast", False)
     df_with_selections.loc[0, "Show weather forecast"] = True
 
-    # Get dataframe row-selections from user with st.data_editor
     edited_df = st.data_editor(
         df_with_selections,
         hide_index=True,
@@ -53,7 +66,7 @@ def dataframe_with_selections(df):
     selected_rows = edited_df[edited_df["Show weather forecast"]]
     return selected_rows.drop("Show weather forecast", axis=1)
 
-# Function to bring in NNR data
+# Function to bring in NNR geoJSON data
 @st.cache_data
 def fetch_geojson():
     url = 'https://services.arcgis.com/JJzESW51TqeY9uat/arcgis/rest/services/National_Nature_Reserves_England/FeatureServer/0/query?outFields=*&where=1%3D1&f=geojson'
@@ -61,22 +74,23 @@ def fetch_geojson():
     gdf = gpd.GeoDataFrame.from_features(r.json()["features"], crs='EPSG:4326')
     return gdf
 
+
 # Streamlit application 
 st.set_page_config(page_title='Local Nature Reserve Finder', 
                    layout='wide', 
                   )
 
-st.title('Local Nature Reserve Finders')
+st.title('Local Nature Reserve Finder')
 st.caption("Natural England’s National Nature Reserves (NNR) are designated areas that are managed and conserved to protect and enhance some of the most important habitats, species and geology (see more [here](https://www.gov.uk/government/collections/national-nature-reserves-in-england#:~:text=National%20Nature%20Reserves%20(%20NNRs%20)%20were,'outdoor%20laboratories'%20for%20research.)).\nThis app helps you find local NNR relative to a postcode you can specify. The distance is then calculated (as the crow flies) and suggests reserves that are within distances you can specify.")
 
-with st.expander("Expand to see instructions on using this application"):
+with st.expander("Application instructions"):
     st.write('''
-        Guidlines on using this app:\n
-        1. Input valid UK postcode.\n
-        2. Specify distance from postcode to travel to a NNR (this is as the crow flies distance).\n
-        3. Visualise the reserve on the map. In addition to the map, the following breakdown is also supplied:\n
-            *(i)* A table for local NNR (ordered by locality), alongside other information (downloadable to CSV).\n
-            *(ii)* A five day weather forecast (provided by the Met Office) on temperature, precipitation and wind speed, to make informed decision of which reserve to visit.
+        **Step 1.** Input valid English postcode.\n
+        **Step 2.** Specify maximum travel distance from postcode to a nature reserve (this will be "as the crow flies" distance).\n
+        **Step 3.** (Optional) Change the base map, this can be adjusted to make the nature reserves more visible.\n
+        **Step 4.** Visualise local reserves on the output map. In addition to the map, the following breakdown is also supplied:\n
+        * A table showing the reserves within the distance threshold (ordered by locality), which can be downloaded as a CSV.
+        * A five day weather forecast (provided by the Met Office) on temperature, precipitation and wind speed, to make a more informed decision on which reserve to visit.
     ''')
 
 # An application that helps you find local nature reserves in England. The application suggests a reserve based on locality and short term weather forecasts.')
@@ -87,18 +101,17 @@ gdf = fetch_geojson()
 # Split streamlit app into two columns
 mapping, information = st.columns((3, 2))
 
-# First column, user settings
+# First column: At the top, user settings
 postcode_settings, distance_settings, map_settings = mapping.columns(3)
-
 postcode = postcode_settings.text_input('Please enter valid UK postcode', value="", max_chars=None, key=None, type="default", help=None)
 distance_miles = distance_settings.number_input('Please enter distance (in miles) from postcode', min_value=0, max_value=100, value=15)
-map_type = map_settings.selectbox(label ='Select basemap', options = [
+map_type = map_settings.selectbox(label ='Change basemap (optional)', options = [
     'OpenStreetMap',
     'cartodbpositron',
     'Cartodb dark_matter'
-
 ], index=0)
 
+# setting lat/lon based on postcode 
 try:
     loc = requests.get(f'https://api.postcodes.io/postcodes/{postcode}').json()
     lat = loc['result']['latitude']
@@ -108,31 +121,33 @@ except:
     st.warning('Please enter valid postcode to use this application', icon="⚠️")
     postcode_entered = False
 
-
+# Only running code once postcode has been entered
 if postcode_entered:
 
+    # Get distance from postcode to each polygon
     gdf['distance'] = gdf['geometry'].apply(distance_from_poly, args=(lat, lon))
+    # Check distance within specified distance
     nearby_parks = gdf[gdf['distance'] <= distance_miles]
+    # Filter datafram 
+    nearby_parks = nearby_parks[['NNR_NAME', 'distance']].rename(columns  = {'NNR_NAME': 'Name', 'distance':'distance (miles)'})
 
-    nearby_parks = nearby_parks[['NNR_NAME', 'distance', 'Shape__Area']].rename(columns  = {'NNR_NAME': 'Name', 'distance':'distance (miles)',  'Shape__Area': 'Land area'})
-
-
+    # Folium map
     m = folium.Map(tiles=map_type, location=(lat, lon), zoom_start=9)
+    # Add marker
     folium.Marker(location=(lat, lon), popup="Central Point").add_to(m)
-
+    # Convert distance to meters
     radius_miles = miles_to_meters(distance_miles)
-
-    # Create a CircleMarker to represent the distance
+    # Define circle around marker
     circle_marker = folium.Circle(
-        location=(lat, lon),
-        radius=radius_miles,
-        color='green',
-        fill=False,
-        dash_array='3',  # Set dash_array for a dotted line
-        popup=f"{distance_miles} radius",
-        z_index=0
+    location=(lat, lon),
+    radius=radius_miles,
+    color='green',
+    fill=False,
+    dash_array='3',  # Set dash_array for a dotted line
+    popup=f"{distance_miles} radius",
+    z_index=0
     )
-
+    # Define popup over polygons
     popup = folium.GeoJsonPopup(
         fields=["NNR_NAME", 'distance'],
         aliases=["Name", 'Distance (in miles)'],
@@ -154,8 +169,7 @@ if postcode_entered:
         """,
 
     )
-    # condition = lambda feature: 'blue' if feature['properties']['distance']<distance_miles else 'red'
-
+    # Add polygons
     folium.GeoJson(gdf.to_json(), name='geojson_layer', 
         tooltip=tooltip,
         popup=popup, style_function=lambda feature: {
@@ -165,11 +179,12 @@ if postcode_entered:
             'weight': 0.1
         },).add_to(m)
 
-
+    # Add circle
     circle_marker.add_to(m)
-
+    # Add layer control
     folium.LayerControl().add_to(m)
 
+    # Add map to app 
     with mapping:
         # ref: https://github.com/gee-community/geemap/issues/713
         st.markdown("""
@@ -181,13 +196,15 @@ if postcode_entered:
                 }
                 </style>
                 """, unsafe_allow_html=True)
-
         # Display the map
         folium_static(m, width=1000,height=650)
 
+    # Further information in app
     with information:
+        # Dataframe
         selection = dataframe_with_selections(nearby_parks.sort_values(by = 'distance (miles)', ascending = True).reset_index(drop=True))
 
+    # Get lat/lon list of NNR's within distance threshold
     locations = gdf[gdf.NNR_NAME.isin(selection.Name.to_list())]['geometry'].centroid.to_list()
 
     # Get weather data for selected locations
@@ -227,9 +244,10 @@ if postcode_entered:
 
     forecast_df = pd.concat(locs_forecast)
 
+    # User input type of weather
     weather_type = information.radio('Select weather feature', ['Tempurature (°C)', 'Chance of precipitation (%)', 'Wind speed (mph)'], index=0, horizontal=True)
 
-
+    # Add plotly figure showing weather
     fig = px.line(forecast_df, x ='date', y=weather_type, color='location')
     fig.update_layout(template = 'seaborn', 
     title = 'Five day weather forecast', 
@@ -238,10 +256,11 @@ if postcode_entered:
     legend=dict(
         orientation="h",
         yanchor="bottom",
-        y=-0.6,
+        y=-0.4,
         xanchor="right",
         x=1
     ))
+    # Add Plotly graph to app
     information.plotly_chart(fig, use_container_width=True)
 
 
